c7467ee71e2ae0aa0a3b85bc3715bcf4fce4010a
hrfswsc <john.k.nathan@nasa.gov>
2025-07-06 22:19:36 +0000

    with JN changes for Ch01/01_03b

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -1,14 +1,15 @@
 // Learning C++ 
 // Challenge 01_03
-// Console Interaction, by Eduardo Corpeño 
+// Console Interaction, by Eduardo Corpeño //with JN Changes
 
 #include <iostream>
 #include <string>
 
 int main(){
     std::string str;
+    std::cout<< "Enter your first name: "  << std::flush;
     std::cin >> str;
-    std::cout << str;
+    std::cout<< "Your first name is: " << str << "!" << std::endl;
 
     std::cout << std::endl << std::endl;
     return (0);
