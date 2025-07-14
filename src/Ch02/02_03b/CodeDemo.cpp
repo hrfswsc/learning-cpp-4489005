5b8de39dcd3a97e9221eada92d636759cb492c1b
hrfswsc <john.k.nathan@nasa.gov>
2025-07-14 16:31:54 +0000

    with src//Ch02/02_03b changes

--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -4,8 +4,19 @@
 
 #include <iostream>
 
+int a, b = 5;  // single line comment
+
+/* Multi
+*  line
+*  comment */
+
 int main(){
-    std::cout << "Hi There!" << std::endl;
+    bool my_flag;
+    a = 7;
+    my_flag = false;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "flag = " << my_flag << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
